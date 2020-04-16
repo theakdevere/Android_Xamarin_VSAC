2113cf2b4b6f08c348e478f0e5373602296f015c
tdevere <anthony_devere@hotmail.com>
2020-04-16 15:28:04 -0500

    test script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,3 +2,8 @@
 set -ex
 NODE_VERSION="8.3.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$APPCENTER_SOURCE_DIRECTORY/node-installer.pkg" -v
+
+echo TotalSystemMemory
+grep MemTotal /proc/meminfo
+echo TotalSystemMemoryEnd
+
