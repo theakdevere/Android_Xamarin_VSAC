3b7a277bf1324e35cf2dcdaeb62ab4a7cc487a61
tdevere <anthony_devere@hotmail.com>
2020-04-16 15:31:53 -0500

    awk '/MemTotal/ { print $2 }' /proc/meminfo

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -4,6 +4,6 @@ NODE_VERSION="8.3.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$APPCENTER_SOURCE_DIRECTORY/node-installer.pkg" -v
 
 echo TotalSystemMemory
-grep MemTotal /proc/meminfo
+awk '/MemTotal/ { print $2 }' /proc/meminfo
 echo TotalSystemMemoryEnd
 
