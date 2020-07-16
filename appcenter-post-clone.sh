4db74010af1d3903537d86b1c2bbb7a932755de6
Tony DeVere <tdevere@microsoft.com>
2020-07-16 15:57:25 -0500

    ls AgentToolsDirectory

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env bash
+echo "######################################################"
+echo "Begin appcenter-post-clone.sh"
+echo "######################################################"
+echo "List all Environment Variables"
+echo "######################################################"
+printenv
+echo "######################################################"
+echo "List files in AgentToolsDirectory"
+echo "######################################################"
+
+ls AgentToolsDirectory 
