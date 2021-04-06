deaf71cfd0a2e6a9b3c6676847b008d9c7cd3e48
Tony DeVere <tdevere@microsoft.com>
2021-04-06 14:20:52 -0500

    copied customer settings

---
+++ Configs/ReleaseTest1Prebuild.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+projectDirectory=${PWD}
+# Copy the Manifest
+cd $projectDirectory/Configs/Manifests
+cp -R "AndroidManifest_ReleaseTest21.xml" "$projectDirectory/Properties/AndroidManifest.xml"
+echo "$projectDirectory/Configs/Manifest/AndroidManifest_ReleaseTest1.xml copied to $projectDirectory/Properties/AndroidManifest.xml"
+echo "***** Android Manifest Contents *****"
+echo "$(cat $projectDirectory/Properties/AndroidManifest.xml)" 
+exit
---
+++ Configs/ReleaseTest2Prebuild.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+projectDirectory=${PWD}
+echo "***** TDEVERE: Print Android Manifest Contents Before Copy *****"
+echo "$(cat $projectDirectory/Properties/AndroidManifest.xml)" 
+# Copy the Manifest
+cd $projectDirectory/Configs/Manifests
+cp -R "AndroidManifest_ReleaseTest2.xml" "$projectDirectory/Properties/AndroidManifest.xml"
+echo "$projectDirectory/Configs/Manifest/AndroidManifest_ReleaseTest1.xml copied to $projectDirectory/Properties/AndroidManifest.xml"
+echo "***** TDEVERE: Print Android Manifest Contents AFTER Copy *****"
+echo "$(cat $projectDirectory/Properties/AndroidManifest.xml)" 
+exit
