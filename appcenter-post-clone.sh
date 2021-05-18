035ace8444cc3a18389445483b8faf8d6d57c6e3
Tony DeVere <tdevere@microsoft.com>
2021-05-18 17:30:57 -0500

    post clone script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,17 +2,22 @@
 echo "######################################################"
 echo "Begin appcenter-post-clone.sh"
 echo "######################################################"
-echo "List all Environment Variables"
+#echo "List all Environment Variables"
 echo "######################################################"
-printenv
+#printenv
 #echo "######################################################"
 #echo "List files in AgentToolsDirectory"
 #echo "######################################################"
 #ls $AgentToolsDirectory 
 echo "######################################################"
-echo "START TCPDUMP"
+#echo "START TCPDUMP"
 echo "######################################################"
-sudo tcpdump -G 15 -W 1 -w myfile
+#sudo tcpdump -G 15 -W 1 -w myfile
 echo "######################################################"
-echo "END TCPDUMP"
+#echo "END TCPDUMP"
 echo "######################################################"
+echo "GET: gradle --version"
+gradle --version
+echo "Done: gradle version"
+echo "######################################################"
+echo "End appcenter-post-clone.sh"
