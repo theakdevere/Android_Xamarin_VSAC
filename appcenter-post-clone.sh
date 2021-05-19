90e333988d9d29eb744427b26882d9e9f4f9b7a0
Tony DeVere <tdevere@microsoft.com>
2021-05-19 18:48:08 -0500

    please work

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,3 +1,6 @@
 ﻿#!/usr/bin/env bash
+echo "VSS_NUGET_EXTERNAL_FEED_ENDPOINTS"
+printenv VSS_NUGET_EXTERNAL_FEED_ENDPOINTS
+
 echo "TDEVERE: Install VSAC_Diag"
 nuget install VSAC_Diag -OutputDirectory $APPCENTER_SOURCE_DIRECTORY -Source tdevere_nuget
