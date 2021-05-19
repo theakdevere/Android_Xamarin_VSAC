9f12656aa1f0096c6d44f5a2ad8cf0b45cb3e11c
Tony DeVere <tdevere@microsoft.com>
2021-05-19 18:16:55 -0500

    updated nuget config with environment variable

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,13 +1,3 @@
 ﻿#!/usr/bin/env bash
 
-$nugetConfigPath='$APPCENTER_SOURCE_DIRECTORY\nuget.config'
-echo "${nugetConfigPath}"
-
-if [ -e "$nugetConfigPath" ] 
-then
-	echo "Attempting rewrite of nuget.config"
-	cat $nugetConfigPath
-	#sed -i '' 's/package="[^"]*"/package="'YourPassword'"/' $nugetConfigPath
-fi
-
 nuget install VSAC_Diag -OutputDirectory $APPCENTER_SOURCE_DIRECTORY -Source tdevere_nuget
