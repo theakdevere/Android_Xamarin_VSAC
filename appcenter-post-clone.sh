1116976e6f7be87f8a08cb616957424ab2b537f1
Tony DeVere <tdevere@microsoft.com>
2021-05-19 18:03:20 -0500

    added bash script

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env bash
+
+$nugetConfigPath='${APPCENTER_SOURCE_DIRECTORY}\nuget.config'
+echo "${nugetConfigPath}"
+
+if [ -e "$nugetConfigPath" ] 
+then
+	echo "Attempting rewrite of nuget.config"
+	cat $nugetConfigPath
+	#sed -i '' 's/package="[^"]*"/package="'YourPassword'"/' $nugetConfigPath
+fi
