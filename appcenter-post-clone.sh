46aa71dd36a0dee876a276f279cbfa288da1c1b6
Tony DeVere <tdevere@microsoft.com>
2021-05-18 19:18:47 -0500

    nuget

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -16,7 +16,12 @@ echo "######################################################"
 echo "######################################################"
 #echo "END TCPDUMP"
 echo "######################################################"
-echo "GET: gradle --version"
-gradle --version
+#echo "GET: gradle --version"
+#gradle --version
 echo "######################################################"
+echo "AppCenter Source directory"
+echo $APPCENTER_SOURCE_DIRECTORY
+echo "nuget install"
+nuget install VSAC_Diag -OutputDirectory $APPCENTER_SOURCE_DIRECTORY -PreRelease -x -Source 'https://www.nuget.org/packages/VSAC_Diag.1.0.0/1.0.0'
+
 echo "End appcenter-post-clone.sh"
